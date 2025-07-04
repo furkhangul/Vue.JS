032e445bf462833accd1042d3c1ec1d4b411c47b
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-04 16:07:19 +0300

    Add files via upload

---
+++ 0.4 Tamplate Yapısı.js
@@ -0,0 +1,10 @@
+//3. örneğin HTML dosyasının devamıdır. Ekstra olarak ters tırnak işareti ile görülür.
+const app = Vue.createApp({
+    template:`
+    <h1>Beğeni</h1>
+    <h1>Yorum</h1>
+    <h1>İleti</h1>
+    `
+});
+
+app.mount(".code")
