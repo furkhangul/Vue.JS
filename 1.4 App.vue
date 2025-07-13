4e5c399313daa0b2ba67e3686a771ca3798f52b2
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-13 23:36:05 +0300

    14

---
+++ 1.4 App.vue
@@ -0,0 +1,31 @@
+<template>
+    <AppTitle :titleText1="text"/>
+    <AppTitle :titleText2="text2"/>
+</template>
+
+<script>
+
+import AppTitle from './components/Baslik.vue';
+export default {
+  name: 'App',
+  components: {
+    AppTitle
+  },
+  data(){
+    return{
+      text:"FurOfTheWeak",
+      text2:"Furkan Gül"
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
