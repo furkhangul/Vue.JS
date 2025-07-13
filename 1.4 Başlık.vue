24160ea12fa6c9386eb579e13eab3b160e804f78
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-13 23:35:44 +0300

    14

---
+++ 1.4 Başlık.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3>{{titleText1}}</h3>
+  <h4>{{titleText2 }}</h4>
+</template>
+
+<script>
+  export default{
+    name: "AppTitle",
+    props:["titleText1","titleText2"],
+    data(){
+      return{
+      }
+    }
+  }
+</script>
+
+<style>
+h3{
+  font-size: 30px;
+  color:aqua;
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
