6ebe87fc381387f1824471c5f9d8180a7110ead5
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-15 23:32:21 +0300

    15

---
+++ 1.5 Componentler Arası Event Aktarımı -  Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <div @click="clean">
+        <h3>Temizlendi</h3>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"ButtonAdd",
+
+        data(){
+            return{
+
+            }
+        },
+    methods:{
+        clean(){
+            this.$emit("clean")
+        }
+    }
+    }
+</script>
+
+<style>
+
+</style>
