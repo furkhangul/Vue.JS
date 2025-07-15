54785d57b5d666ef37594e28c58dbcbfac5ee48c
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-15 23:31:44 +0300

    15

---
+++ 1.5 Componentler Arası Event Aktarımı - App.vue
@@ -0,0 +1,41 @@
+<template>
+    <AppTitle :titleText1="text"/>
+    <AppTitle :titleText2="text2"/>
+    <ButtonAdd @clean="cleanInput" />
+    <input type="text" ref="inputText">
+</template>
+
+<script>
+
+import AppTitle from './components/Baslik.vue';
+
+import ButtonAdd from './components/Button.vue';
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+    ButtonAdd
+  },
+  data(){
+    return{
+      text:"FurOfTheWeak",
+      text2:"Furkan Gül"
+    }
+  },
+  methods:{
+    cleanInput(){
+      this.$refs.inputText.value = "";
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
