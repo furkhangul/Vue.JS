d1697a711294715327d71d1aaf095eda89c98dd7
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-17 18:11:05 +0300

    18

---
+++ 1.8 Checkbox Data Binding - Form.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <input type="checkbox" id="checkbox1" v-model="checked" />
+        <label for="checkbox1"> Onay</label>
+        <p>{{ checked ? "Kabul Ediyorum" : "Kabul Etmiyorum" }}</p>
+    </form>
+</template>
+
+<script>
+    export default
+    {
+        name:"FormData",
+        data(){
+            return{
+                checked: true,
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
