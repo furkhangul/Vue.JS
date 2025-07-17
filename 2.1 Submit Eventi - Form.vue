c16ad4fd56f30ca50184419124fd0ed0ab902025
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-17 20:22:56 +0300

    21

---
+++ 2.1 Submit Eventi - Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <!-- .prevent uzantısı JS de olduğu üzere sayfayı yenilenmesini engelliyor.-->
+        <button @click.prevent="sendData">Gönder</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormData",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    sendData(){
+        console.log("Veriler Gönderildi")
+    }
+  }
+};
+</script>
