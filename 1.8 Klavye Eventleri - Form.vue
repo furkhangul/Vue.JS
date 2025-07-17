9b3449f8a9f9343991106c1f6be7ea9b014fe200
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-17 20:01:46 +0300

    18

---
+++ 1.8 Klavye Eventleri - Form.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent>
+    <h3>Öğrenciler</h3>
+    <input type="text" v-model="student" @keyup="addStudent">
+  </form>
+
+  <div>
+    <h4>Eklenen Öğrenciler:</h4>
+    <div v-for="student in students" :key="student">
+      {{ student }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormData",
+  data() {
+    return {
+      student: "",
+      students: []
+    };
+  },
+  methods: {
+    addStudent(e) {
+      if (e.key === "," && this.student.trim()) {
+        const name = this.student.split(",")[0].trim();
+        if (name && !this.students.includes(name)) {
+          this.students.push(name);
+        }
+        this.student = "";
+      }
+    }
+  }
+};
+</script>
