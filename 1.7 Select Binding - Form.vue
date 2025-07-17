3f3131bf79a059c9e2e38cb11d3490bdaf2cec27
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-17 17:59:40 +0300

    17

---
+++ 1.7 Select Binding - Form.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <select v-model="renkler">
+            <option>Mavi</option>
+            <option>Gri</option>
+            <option>Beyaz</option>
+        </select>
+        <p>{{ renkler }}</p>
+    </form>
+</template>
+
+<script>
+    export default
+    {
+        name:"FormData",
+        data(){
+            return{
+                renkler:""
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
