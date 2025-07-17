dc6e8371d5bb44d91bd37f7a90e25db2b4e27e83
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-17 20:23:25 +0300

    21

---
+++ 2.1 Submit Eventi - App.vue
@@ -0,0 +1,17 @@
+<template>
+    <FormData />
+</template>
+
+<script>
+import FormData from "./components/Form.vue"
+  export default{
+    name:"App",
+    components:{
+      FormData
+    }
+  }
+</script>
+
+<style>
+
+</style>
