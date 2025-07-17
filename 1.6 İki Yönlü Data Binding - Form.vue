f105521bf0c01d5575e3fd68ca79eb850d0f7bbf
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-17 17:52:05 +0300

    16

---
+++ 1.6 İki Yönlü Data Binding - Form.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <!--Kullanıcının girdiği verileri, Vue bileşenindeki bir değişkene otomatik olarak bağlar ve günceller-->
+        <input type="text" v-model="text"/>
+        <p>{{text}}</p>
+    </form>
+</template>
+
+<script>
+    export default
+    {
+        name:"FormData",
+        data(){
+            return{
+                text:"deneme"
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
