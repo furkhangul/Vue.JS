88b807aff72c1a641979a4ed093bd6938ae4d251
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-19 10:22:28 +0300

    22

---
+++ 2.2 Router Kurulumu ve dosya açma işlemleri.js
@@ -0,0 +1,23 @@
+/*Burada kurulum için gereken adımlar:
+    Öncelikle yeni bir klasör açıyoruz.
+    Ardından terminale giriyoruz.
+    Terminale : "vue create router-app" emrini veriyoruz.
+    Karşımıza çıkan seçeneklerden "Manually select features" seçeneğini clikliyoruz.
+    Default gelen Lİnter/Formetter seçenğini clikten çıkarıyoruz.
+    Router seçeneğini de space tuşu ile aktifleştiriyoruz.
+    Ardından gelen panelde 3x i işareteyip yes seçeneğini, In dedicated config files seçeneğini ve N seçeneğini işaretliyoruz.
+
+    Kurulum başlıyor.
+    Kurulum tamamladıktan sonra:
+
+    cd router-app
+    npm run serve 
+    komutları ile projeyi ayağa kaldırıyoruz.
+
+    Web sitemize giriyoruz.
+    Girdiğimiz siteyi inceleme moduna alıp ağ kısmına baktığımız zaman app.js adından ve bunun gibi birkaç dosya mevcut.
+
+    Biz web sitede bulunan linke basıp about sayfasına geçersek ağ kısmına ekstra about.js adından bir dosya daha gelecektir.
+    Bu basit bir vue mantığıdır. İlerde anlatılacak.
+*/
+    
