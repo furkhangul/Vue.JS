736db85434180c733527ba253b5e9e6861f017cd
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-19 16:00:55 +0300

    26

---
+++ 2.6 Route Params - StudentsInfo.vue
@@ -0,0 +1,24 @@
+    <template>
+        <div>
+            <h3>Öğrenci Bilgi Sayfası</h3>
+            <p>Öğrenci ID'si: {{StudentId}}</p>
+        </div>
+    </template>
+    <script>
+    export default{
+        name:"studentInfo",
+        data(){
+            return{
+
+                //Burada Url den id bilgisini almak için kullanılıyor.
+                //.../student/2 yaptığımız zaman 2 numaralı id'ye sahip kullanıcı sahneleniyor.
+                //params urldeki değişken kısmını tanımlar.
+                
+                StudentId : this.$route.params.id
+            }
+        }
+    }
+    </script>
+    <style>
+
+    </style>
