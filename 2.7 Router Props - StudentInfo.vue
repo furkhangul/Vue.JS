94b68ac81273d196c143e155f8266bced4c5c3ae
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-20 14:24:40 +0300

    27

---
+++ 2.7 Router Props - StudentInfo.vue
@@ -0,0 +1,21 @@
+    <template>
+        <div>
+            <h3>Öğrenci Bilgi Sayfası</h3>
+            <p>Öğrenci ID'si: {{StudentId}}</p>
+        </div>
+    </template>
+    <script>
+    export default{
+        name:"studentInfo",
+        props:["StudentId"]
+    }
+
+    //Propslar, properties kelimesinin kısaltması yani özellktir. Bir üst bileşenden alt bileşene veri aktarmak için kullanılır.
+
+    //Bir bileşeni daha esnek, yeniden kullanılabilir ve dinamik hale getirir.
+    //Vue bileşenleri arası iletişim sağlar.
+    //Parents -> child yönünde
+    </script>
+    <style>
+
+    </style>
