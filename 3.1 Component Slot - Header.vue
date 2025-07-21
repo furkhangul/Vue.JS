3abbb8b5a8c57d0c05a7aa440c4d085cd0d3127f
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-21 13:47:41 +0300

    31

---
+++ 3.1 Component Slot - Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3 class="head">{{content}}</h3>
+        <!--YERİNE ALTERNATİF OLARAK-->
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default{
+        props: ['content']
+    }
+</script>
+<style>
+.head{
+    margin:20px;
+    padding:20px;
+    font-size: 30px;
+    font-weight: bold;
+    color:black;
+    border: 0cap;
+    }
+</style>
