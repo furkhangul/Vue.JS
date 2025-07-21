cd27cef3bb57f411dc8bbd3ed4fdd31ccd9dc26d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-21 12:58:03 +0300

    29

---
+++ 2.9 Redirect Catch All - NotFound.vue.js
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3 :style="style">404 Found Error</h3>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            style:"text-align:center;color:black; fount-size:30px;margin:20px; padding:30px;"
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
