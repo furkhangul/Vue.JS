33c44584760da7508996a2f2d3940ea681b05c2f
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-25 15:53:34 +0300

    34

---
+++ 3.4 Uygulama 2 - Temizleme İşlemleri - Projeler.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+    export default{
+        name:"Projeler",
+
+        data(){
+            return{
+
+                
+            }
+
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
