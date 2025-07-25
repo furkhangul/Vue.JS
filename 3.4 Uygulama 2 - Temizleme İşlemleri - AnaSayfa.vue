2e78ffcde397403ebfc8d24872f97795a2b7f614
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-25 15:52:36 +0300

    34

---
+++ 3.4 Uygulama 2 - Temizleme İşlemleri - AnaSayfa.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>
+    Ana Sayfa
+  </h1>
+</template>
+<script>
+
+import AnaSayfa from '@/components/ana.vue'
+
+export default {
+  name: 'Ana Sayfa',
+  components: {
+    AnaSayfa
+  }
+}
+</script>
