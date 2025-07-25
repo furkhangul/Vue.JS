385f60c4b8c8970833cb679c599a14103d861aa3
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-25 16:12:07 +0300

    35

---
+++ 3.5 Uygulama 2 - Data - Kurulum.js
@@ -0,0 +1,20 @@
+/*
+Veriyi hem yazacağımız hem de okuyacağımız bir veri tabanın ihtiyacımız var.
+Daha sonraki projelerde farklı veri tabanı sistemleri kullanacağız fakat bu şuan json kullanmamız daha uygun olacaktır.
+
+Localde açacağız bu json dosyasını.
+
+bunun için json server yazarsak karşımıza çıkan .
+
+https://www.npmjs.com/package/json-server
+Adlı web siteden devam edersek.
+
+npm install json-server
+
+npm indirme dosyası ile terminale bu komutu girmemiz gerekmektedir.
+
+Komutu girdiğimiz zaman veriler global dosyalara kayıt ediliyor. İstediğimiz yerden çekip kullanabiliriz.
+
+Kullanmak için ise proje doyasının içine  "Data" adında bir dosya daha açıp bu data içerisinde bir belge oluşturup bu belgeye
+"db.json" şeklinde tanımlarsak indirme işlemi tamamlanmış olur.
+*/
