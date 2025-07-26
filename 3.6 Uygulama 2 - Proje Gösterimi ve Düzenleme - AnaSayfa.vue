b835970d3a4419c73be2cfa1b7f9e1cd78d3bb4c
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-26 10:10:50 +0300

    36

---
+++ 3.6 Uygulama 2 - Proje Gösterimi ve Düzenleme - AnaSayfa.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <Veri :project="project"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Veri from '../components/VeriGonder.vue'
+export default {
+    data(){
+      return{
+        projects : [
+
+        ]
+      }
+    },
+    components: {
+      Veri
+    },
+
+    mounted() {
+      fetch('http://localhost:3000/Projeler')
+      .then( res => res.json())
+      .then(data  => this.projects = data)
+      .catch(err => console.log(err.message))
+    }
+}
+</script>
