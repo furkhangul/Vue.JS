12e73d462288b309dd650883d63d4dbac190c2fd
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-31 00:48:03 +0300

    47

---
+++ 4.7 Uygulama 2 - wievs - AnaSayfa.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+<Filtre @filtrele="current = $event" :current="current" />
+    <div v-if="projects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+        <Veri :project="project" @delete="handleDelete" @bittiDegisti="handleBittiGuncelle"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Veri from '../components/VeriGonder.vue'
+import Filtre from './Filtre.vue'
+export default {
+  data() {
+    return {
+      projects: [],
+      current:"all"
+    }
+  },
+  components: {
+    Veri,
+    Filtre
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => project.id !== id)
+    },
+    handleBittiGuncelle(id) {
+      this.projects = this.projects.map(p => {
+        if (p.id === id) {
+          return { ...p, bitti: !p.bitti };
+        }
+        return p;
+      });
+    }
+  },
+  computed:{
+    filteredProjects(){
+      if(this.current === "complate"){
+        return this.projects.filter((project) => project.bitti)
+      }
+     if(this.current === "ongoing"){
+        return this.projects.filter((project) => !project.bitti)
+      }
+      else{
+        return this.projects
+      }
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/Projeler')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  }
+}
+</script>
