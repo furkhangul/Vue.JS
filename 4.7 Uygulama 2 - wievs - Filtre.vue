63631f8b4e35595b3e4870f92c6c6a30dbe576d7
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-31 00:49:01 +0300

    47

---
+++ 4.7 Uygulama 2 - wievs - Filtre.vue
@@ -0,0 +1,37 @@
+,<template>
+  <nav class="filtreleme">
+    <button @click="filtrele('all')" :class="{ active: current === 'all' }">Tümü</button>
+    <button @click="filtrele('complate')" :class="{ active: current === 'complate' }">Bitirilmiş</button>
+    <button @click="filtrele('ongoing')" :class="{ active: current === 'ongoing' }">Bitirilmemiş</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['current'],
+  methods: {
+    filtrele(by) {
+      this.$emit('filtrele', by);
+    }
+  }
+};
+</script>
+
+<style>
+.filtreleme button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  text-transform: uppercase;
+  font-size: 12px;
+  margin: 8px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  cursor: pointer;
+}
+
+.filtreleme button.active {
+  color: #555;
+}
+</style>
