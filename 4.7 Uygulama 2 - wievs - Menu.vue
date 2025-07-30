7596eb217cda201ee6caeba90c7ac2fc42fc84a4
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-07-31 00:49:24 +0300

    47

---
+++ 4.7 Uygulama 2 - wievs - Menu.vue
@@ -0,0 +1,35 @@
+<template>
+<nav class="manu">
+    <RouterLink :to="{path:'/'}">Ana Sayfa</RouterLink> | 
+    <RouterLink :to="{path:'/ekle'}">Proje Ekleme</RouterLink>
+</nav>
+
+    
+</template>
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+.manu{
+    text-align: center;
+    margin:40px auto;
+    color:aliceblue;
+}
+.manu a {
+    display: inline-block;
+    text-decoration:none;
+    font-weight: bold;
+    color:#999;
+}
+a.router-link-active{
+    border-bottom: 2px solid rgb(33, 91, 33);
+    padding-bottom:4px;
+}
+</style>
