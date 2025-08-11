3b66a045e988370f324f2c55e395e9a04f78d84b
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-08-11 17:04:07 +0300

    48

---
+++ 4.8 Composition API.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<script>
+
+export default {
+  /*
+  Options API
+
+  *Vue 2'den beri kullanılan geleneksel yöntemdir.
+  *Bileşen özellikleri (data,methods,computed,watch gibi) anahtar kelimeler ile tanımlanır.
+  *Her özellik kendi bölümünde yazılır.
+  *Daha okunabilir ve yeni başlayanlar için daha anlaşılırdır.
+  *Ancak büyük projelerde ilgili kodlar farklı bölümlere dağıtılır, bu yüzden bakım zorlaşır. 
+  
+
+  Composition API
+
+  *Vue 3 ile gelen modern yöntemdir.
+  *Tüm mantık setup() fonksiyonu içinde tanımlaır.
+  *Değişkenler ref veya reactive ile reaktif hale getirilir, fonksiyonlar doğrudan JS fonksiyonu gibi tanımlanır.
+  *this kullanılmaz, değişkene doğrudan erişilir.
+  *İlgili mantık aynı yerde gruplanabilir, bu sayede büyük projelerde kodun yönetimi kolaylaşır.
+  *Mantık blokları, composable fonksiyonları ile farklı bileşenler arasında kolayca paylaşılabilir.  
+  */
+
+
+  /*
+  Otions API -> Kod "özellik türüne" göre düzenlenir.
+
+  Composition API -> Kod "mantık işlevine" göre düzenlenir.
+  
+  */
+}
+</script>
